4c916258c1d519d2fd700f786235e78120eecb90
apurva24july <115028777+apurva24july@users.noreply.github.com>
2024-08-06 11:31:24 +0530

    Add files via upload

---
+++ blokchain.py
@@ -0,0 +1,57 @@
+import hashlib
+import time
+
+class Block:
+    def __init__(self, index, timestamp, data, previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        sha = hashlib.sha256()
+        sha.update((str(self.index) + str(self.timestamp) + str(self.data) + self.previous_hash).encode())
+        return sha.hexdigest()
+
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+
+    def create_genesis_block(self):
+        return Block(0, time.time(), "Genesis Block", "0")
+
+    def get_latest_block(self):
+        return self.chain[-1]
+
+    def add_block(self, new_block):
+        new_block.previous_hash = self.get_latest_block().hash
+        new_block.hash = new_block.calculate_hash()
+        self.chain.append(new_block)
+
+    def is_chain_valid(self):
+        for i in range(1, len(self.chain)):
+            current_block = self.chain[i]
+            previous_block = self.chain[i-1]
+
+            if current_block.hash != current_block.calculate_hash():
+                return False
+
+            if current_block.previous_hash != previous_block.hash:
+                return False
+
+        return True
+
+# Testing the blockchain
+blockchain = Blockchain()
+blockchain.add_block(Block(1, time.time(), "Block 1 Data", blockchain.get_latest_block().hash))
+blockchain.add_block(Block(2, time.time(), "Block 2 Data", blockchain.get_latest_block().hash))
+
+for block in blockchain.chain:
+    print(f"Index: {block.index}")
+    print(f"Timestamp: {block.timestamp}")
+    print(f"Data: {block.data}")
+    print(f"Hash: {block.hash}")
+    print(f"Previous Hash: {block.previous_hash}\n")
+
+print("Blockchain valid?", blockchain.is_chain_valid())
